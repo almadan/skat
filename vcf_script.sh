9b3b7afc355753493185011b722ba4878efb2bf7
alma <al3327da-s@student.lu.se>
2018-06-15 15:58:06 +0200

    lagt till vcf script

---
+++ vcf_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Kommando för att hämta hem kromosom vcf
+vcftools --gzvcf /home/alma/cbio3/projects/Britt-Marie/WES/results/intermediate/FinalSet.recode.vcf.gz --chr 1 --recode --recode-INFO-all --stdout | gzip -c > ~/projekt/chr1.vcf.gz
+
+#Kommando för att te bort duplikat
+vcftools --gzvcf ~/projekt/chr1.vcf.gz --remove /home/alma/cbio3/projects/Britt-Marie/WES/results/intermediate/FinalSetMissingIndv.below95 --remove /home/alma/cbio3/projects/Britt-Marie/WES/results/intermediate/MDS/MDS2/DuplicatesToRemove_uniq.ids --stdout | gzip -c > ~/projekt/chr1a.vcf.gz
+
